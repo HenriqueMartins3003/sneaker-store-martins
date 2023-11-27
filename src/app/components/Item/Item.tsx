55509098ed91a5f2ff52ba17824bb7e103e0ee9e
Henrique Martins <hiikemartins@gmail.com>
2023-11-27 19:32:23 -0300

    Feat: add Item tsx and timeout interval and bug fixes on ItemList

--- src/app/components/Item/Item.tsx
+++ src/app/components/Item/Item.tsx
@@ -1,13 +1,17 @@
+import { GetItemsInterface } from "@/app/interfaces/Items.Interface";
 import Image from "next/image";
 
 import React from "react";
 
-interface Props {
-  imageURL: string;
-  rightImage: string;
-  leftImage: string;
-}
-const Item = ({ imageURL, leftImage, rightImage }: Props) => {
+const Item = ({
+  principalImage,
+  leftImage,
+  rightImage,
+  description,
+  id,
+  price,
+  title,
+}: GetItemsInterface) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex mb-2 mx-auto">
@@ -19,7 +23,7 @@ const Item = ({ imageURL, leftImage, rightImage }: Props) => {
           className="overflow-hidden mx-1"
         />
         <Image
-          src={imageURL}
+          src={principalImage}
           alt="Imagem sneaker Space Jam"
           width={300}
           height={300}
@@ -33,15 +37,9 @@ const Item = ({ imageURL, leftImage, rightImage }: Props) => {
           className="overflow-hidden mr-1"
         />
       </div>
-      <p className="text-sm text-neutral-950">
-        {" "}
-        Tênis Nike Air Force 1 07 x Space Jam Masculino Clássico e atemporal, o
-        tênis Nike Air Force 1 07 é um modelo que nunca sai de moda! Construído
-        com silhueta de cano baixo, o tênis apresenta um visual clean e
-        versátil, com cabedal todo feito de couro. Os detalhes incluem Swooshes
-        laterais e a assinatura Nike Air estampada nas etiquetas das línguas e
-        na região dos calcanhares. Perfeito para compor qualquer outfit!{" "}
-      </p>
+      <h4>{title}</h4>
+      <p>{price}</p>
+      <p className="text-sm text-neutral-950">{description}</p>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ItemList from "./components/ItemListContainer/ItemList";
+import ItemList from "./components/Item/ItemList";
 import { NavBar } from "./components/NavBar/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
